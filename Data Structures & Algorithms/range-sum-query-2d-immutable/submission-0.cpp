54eba6971e0b28feab9f6146ca4cff9b982e1f04
Shashank Raj <shashankraj72777@gmail.com>
2026-07-24 22:10:32 +0530

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.cpp
@@ -0,0 +1,33 @@
+class NumMatrix {
+    vector<vector<int>> rowPrefix;
+
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        rowPrefix.assign(m, vector<int>(n + 1, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowPrefix[i][j + 1] = rowPrefix[i][j] + matrix[i][j];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+
+        for (int i = row1; i <= row2; i++) {
+            sum += rowPrefix[i][col2 + 1] - rowPrefix[i][col1];
+        }
+
+        return sum;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
